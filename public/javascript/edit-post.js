5880a1edfe1813ba129c7eb86bc68a8e435055a4
jsun <jaysun054@gmail.com>
2021-11-03 18:08:18 -0700

    working

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,7 +2,8 @@ async function editFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value.trim();
-    //const post_text = document.querySelector('textarea[name="post-text"]').value;
+    const post_text = document.querySelector('textarea[name="post-text"]').value;
+
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
@@ -10,8 +11,8 @@ async function editFormHandler(event) {
     const response = await fetch(`/api/posts/${id}`, {
         method: 'put',
         body: JSON.stringify({
-            title
-            //post_text
+            title,
+            post_text
         }),
         headers: { 'Content-Type': 'application/json' }
     });
