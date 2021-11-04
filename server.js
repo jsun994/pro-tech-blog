b8f658d8e3bb83eefb486e58a47e0836b5ec642a
jsun <jaysun054@gmail.com>
2021-11-03 18:49:32 -0700

    set cookie max age to 5 mins

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: { originalMaxAge: 150000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
